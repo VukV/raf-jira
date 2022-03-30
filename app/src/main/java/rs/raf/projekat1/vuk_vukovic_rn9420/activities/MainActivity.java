e5c5a46a86b6bffcfb5f76b1d6aada5db6d4382a
VukV <vuk.vukovic.2000@gmail.com>
2022-03-30 22:01:53 +0200

    Login activity done

--- app/src/main/java/rs/raf/projekat1/vuk_vukovic_rn9420/activities/MainActivity.java
+++ app/src/main/java/rs/raf/projekat1/vuk_vukovic_rn9420/activities/MainActivity.java
@@ -1,16 +1,47 @@
 package rs.raf.projekat1.vuk_vukovic_rn9420.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import rs.raf.projekat1.vuk_vukovic_rn9420.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String PREF_LOGIN_INFO = "loginInfo";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        splashScreen.setKeepOnScreenCondition(() -> {
+            start();
+            return false;
+        });
+
         setContentView(R.layout.activity_main);
+    }
+
+    private void start(){
+        finish();
+
+        if (checkLogin()){
+            //TODO
+        }
+        else {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        }
+    }
+
+    private boolean checkLogin(){
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        String username = sharedPreferences.getString(PREF_LOGIN_INFO, "");
+
+        return !username.equals("");
     }
 }
---
+++ app/src/main/java/rs/raf/projekat1/vuk_vukovic_rn9420/data/LoginData.java
@@ -0,0 +1,7 @@
+package rs.raf.projekat1.vuk_vukovic_rn9420.data;
+
+public class LoginData {
+
+    public static final String ADMIN_PASSWORD = "admin123";
+    public static final String USER_PASSWORD = "user123";
+}
